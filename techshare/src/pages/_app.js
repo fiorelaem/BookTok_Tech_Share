c651e7ba4877466b6c128ab25e71f1f3564d2b89
fiorelaem <fiorela.e.martinez@gmail.com>
2023-04-05 15:43:50 -0500

    style update

--- techshare/src/pages/_app.js
+++ techshare/src/pages/_app.js
@@ -1,13 +1,43 @@
-// import '@/styles/globals.css'
 import Link from 'next/link' 
+import '@/styles/index.css'
+import { BsGithub } from 'react-icons/bs';
 
 export default function App({ Component, pageProps }) {
   return <>
-    <Link href="/camera">Camera</Link>
+    {/* <Link href="/camera">Camera</Link>
     <br></br>
     <Link href="/screenshot">Screenshot</Link>
     <br></br>
-    <Link href="/video">Video</Link>
+    <Link href="/video">Video</Link> */}
+
+    <header>
+      <nav className='navbar'>
+        <Link href="/">
+          <img src="/BookTokLogo.png" alt="BookTok" className='navbar__logo' />
+        </Link>
+
+        <ul className='navbar__menu'>
+          <li>
+            <Link href="/camera"> WebCam</Link>
+          </li>
+
+          <li>
+            <Link href="/screenshot"> Screenshot</Link>
+          </li>
+
+          <li>
+            <Link href="/video"> Video</Link>
+          </li>
+        </ul>
+
+        <ul className='navbar__icons'>
+          <li>
+            <a href="https://github.com/csci5117s23/ReactWebcamTechShare"><BsGithub /></a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
     <Component {...pageProps} />  
   </>
 }
