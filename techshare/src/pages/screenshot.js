a2a6371950a5b5d6838ba2ed1f4210c0321172fd
fiorelaem <fiorela.e.martinez@gmail.com>
2023-04-10 01:33:25 -0500

    merge conflict

--- techshare/src/pages/screenshot.js
+++ techshare/src/pages/screenshot.js
@@ -1,7 +1,8 @@
 // Code taken from https://blog.openreplay.com/capture-real-time-images-and-videos-with-react-webcam/
 // with some slight modifications for our use in our tech share
 
-import React, { useCallback, useRef, useState } from "react";
+
+import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 function WebcamImage() {
@@ -20,40 +21,20 @@ function WebcamImage() {
   }, [webcamRef]);
 
   return (
-    <div>
-      <h1 class='centered'>Screenshot</h1>
-      
-      {img === null ? (
-        <>
-        <div class='Webcam'>
-          <Webcam
-            audio={false}
-            mirrored={true}
-            height={400}
-            width={400}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-          />
-        </div>
-        <br></br>
-        <div  class='centered'>
-          <button onClick={capture}>Capture photo</button>
-        </div>
-        </>
-      ) : (
-        <>
-        <div class='Webcam'>
-          <img src={img} alt="screenshot" />
-        </div>
-          <br></br>
-        <div  class='centered'> 
-          <button onClick={() => setImg(null)}>Retake</button>
-        </div>
-        </>
+    <div className="Container">
+      <div className="CameraContainer">
+        <Webcam
+          mirrored={true}
+          videoConstraints={videoConstraints}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+        />
+        <button onClick={capture}>Capture photo</button> 
+      </div>
+      {img && (
+          <img src={img} alt="capturedPhoto"/>
       )}
-      
-    </div>
+    </div> 
   );
 }
 
