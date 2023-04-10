056d74ca28601fe8759d66c3ae0ac4d03a551f13
hoin0784 <hoin0784@gmail.com>
2023-04-10 04:19:19 -0500

    Added comment in (mic.js)

--- techshare/src/pages/mic.js
+++ techshare/src/pages/mic.js
@@ -1,50 +1,55 @@
 // Code taken from https://www.assemblyai.com/blog/react-speech-recognition-with-react-hooks/
 // with some slight modifications for our use in our tech share
 
+// npm install --save react-mic
 
 import MicRecorder from "mic-recorder-to-mp3"
 import { useState, useRef, useCallback } from "react"
 
 export default function MicAudio() {
 
-  const recorder = useRef(null);
-  const audioPlayer = useRef(null);
+  const recorder = useRef(null);                          // Used useRef to store img/video/audio
+  const audioPlayer = useRef(null);                       // Used useRef to refer updated audio
 
-  const [blobURL, setBlobUrl] = useState(null);
-  const [audioFile, setAudioFile] = useState(null);
-  const [isRecording, setIsRecording] = useState(null);
+  const [blobURL, setBlobUrl] = useState(null);           // Take a look at the stopRecording func
+  const [audioFile, setAudioFile] = useState(null);       // To store the AudioFile
 
-  const startRecording = useCallback(() => {
-    setIsRecording(true);
-    recorder.current = new MicRecorder({ 
-      bitRate: 128 
-    });
-    recorder.current.start();
-  }, [recorder, setIsRecording]);
+  const [isRecording, setIsRecording] = useState(null);   // when the user click start button setIsRecording(true)
+                                                          // when the user click stop button setIsRecording(false)
 
 
-  const stopRecording = useCallback(() => {
-    recorder.current.stop();
+  const startRecording = useCallback(() => {              // This function is called when the user starts recording.
+    setIsRecording(true);                                 
 
-    recorder.current.getMp3()
+    recorder.current = new MicRecorder();                 // Store the MicRecorder object to recorder.current (null before assinging)
+    recorder.current.start();                             // Start Recording !
+
+  }, [recorder, setIsRecording]);                         // If the function setIsRecording or recorder variable is changed then,
+                                                          // It is called again.
+  
+  const stopRecording = useCallback(() => {               // This functin is called when the user stops recording.
+    recorder.current.stop()                               // Stop Recording
+    recorder.current.getMp3()                             // Get the buffer and blob from new Blob (see the getMp3 definition
     .then(([buffer, blob]) => {
-      const file = new File(buffer, "audio.mp3", {
-        type: blob.type,
-        lastModified: Date.now(),
-      })
-      const newBlobUrl = URL.createObjectURL(blob)
+      const file = new File(buffer, "audio.mp3")          
+      const newBlobUrl = URL.createObjectURL(blob)        // create URL for downloading 
+
       setBlobUrl(newBlobUrl)
       setIsRecording(false)
       setAudioFile(file)
     })
-  }, [recorder, setIsRecording, setAudioFile, setBlobUrl]);
+    .catch((e) => console.log(e));
+    
+  }, [recorder, setIsRecording, setAudioFile, setBlobUrl]);       // If the function setIsRecording / setAudioFile / setBlobUrl or recorder variable is changed then,
+                                                                  // It is called again.
 
+  // The reason for disabled : To disallow two button(Start/Stop) clicks                                                    
   return (
-    <div class='centered'>
+    <div className="centered">
       <h1>Mic Recorder</h1>
       <audio ref={audioPlayer} src={blobURL} controls='controls' />
       <div>
-        <button disabled={isRecording} onClick={startRecording}>
+        <button disabled={isRecording} onClick={startRecording}>    
           START
         </button>
         <button disabled={!isRecording} onClick={stopRecording}>
@@ -54,63 +59,3 @@ export default function MicAudio() {
     </div>
   )
 }
-
-// Implementation of microphone without using callback React hooks:
-
-// import MicRecorder from "mic-recorder-to-mp3"
-// import { useEffect, useState, useRef } from "react"
-
-// const App = () => {
-//   // Mic-Recorder-To-MP3
-//   const recorder = useRef(null) //Recorder
-//   const audioPlayer = useRef(null) //Ref for the HTML Audio Tag
-//   const [blobURL, setBlobUrl] = useState(null)
-//   const [audioFile, setAudioFile] = useState(null)
-//   const [isRecording, setIsRecording] = useState(null)
-
-//   useEffect(() => {
-//     //Declares the recorder object and stores it inside of ref
-//     recorder.current = new MicRecorder({ bitRate: 128 })
-//   }, [])
-
-//   const startRecording = () => {
-//     // Check if recording isn't blocked by browser
-//     recorder.current.start().then(() => {
-//       setIsRecording(true)
-//     })
-//   }
-
-//   const stopRecording = () => {
-//     recorder.current
-//       .stop()
-//       .getMp3()
-//       .then(([buffer, blob]) => {
-//         const file = new File(buffer, "audio.mp3", {
-//           type: blob.type,
-//           lastModified: Date.now(),
-//         })
-//         const newBlobUrl = URL.createObjectURL(blob)
-//         setBlobUrl(newBlobUrl)
-//         setIsRecording(false)
-//         setAudioFile(file)
-//       })
-//       .catch((e) => console.log(e))
-//   }
-
-//   return (
-//     <div>
-//       <h1>React Mic</h1>
-//       <audio ref={audioPlayer} src={blobURL} controls='controls' />
-//       <div>
-//         <button disabled={isRecording} onClick={startRecording}>
-//           START
-//         </button>
-//         <button disabled={!isRecording} onClick={stopRecording}>
-//           STOP
-//         </button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
